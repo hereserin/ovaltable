aea599446645d499fc8443f47d854568fd1c894a
App Academy Student <appacademy@Apps-Mac-mini-2.local>
2018-09-11 18:25:23 -0400

    Fix syntax error

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  validates :username, :session_token uniqueness: true, presence: true
+  validates :username, :session_token, uniqueness: true, presence: true
   validates :password_digest, presence: true
   validates :password, length: { minimum: 6, allow_nil: true }
   attr_reader :password
