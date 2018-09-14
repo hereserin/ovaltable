2c751504fad25198e904cd1e2bae095b67ae60b1
Erin Marshall <erin.e.mars@gmail.com>
2018-09-14 03:28:42 -0400

    Begin search restaurants form

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -4,6 +4,7 @@ import GreetingContainer from './greeting/greeting_container';
 import LoginFormContainer from './session/login_form_container';
 import SignupFormContainer from './session/signup_form_container';
 import NavBar from './nav_bar/nav_bar';
+import SearchBar from './search_bar/search_bar';
 import { AuthRoute, ProtectedRoute } from './../util/route_util.jsx';
 import { Route } from 'react-router-dom';
 
@@ -13,7 +14,9 @@ const App = () => {
       <header>
         <NavBar />
         <h1>Hi.</h1>
+        <SearchBar />
       </header>
+
 
       <AuthRoute path='/login' component={LoginFormContainer} />
       <AuthRoute path='/signup' component={SignupFormContainer} />
---
+++ frontend/components/search_bar/search_bar.jsx
@@ -0,0 +1,38 @@
+// search bar
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const SearchBar = () => {
+    return (
+      <div>
+        <fieldset className='home-search-box'>
+          <h2>
+            Find your table for any occasion
+          </h2>
+          <input type='date' className="date-search" />
+
+          <select className='time-search'>
+            <option value='11:00'>11:00 AM</option>
+            <option value='11:30'>11:30 AM</option>
+            <option value='12:00'>12:00 PM</option>
+            <option value='12:30'>12:30 PM</option>
+            <option value='13:00'>1:00 PM</option>
+            <option value='13:30'>1:30 PM</option>
+          </select>
+
+          <select className='people-search'>
+            <option value='1'>1 person</option>
+            <option value='2'>2 people</option>
+            <option value='3'>3 people</option>
+          </select>
+
+          <input type='text' placeholder='Search Restaurants' className='search-bar-text'></input>
+
+          <button className='search-submit-button'>Let's go</button>
+        </fieldset>
+      </div>
+    );
+};
+
+
+export default SearchBar;
