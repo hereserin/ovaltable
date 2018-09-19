be267871f0e243f88d50a6aca3492aa27249b6f9
Erin Marshall <erin.e.mars@gmail.com>
2018-09-19 13:19:38 -0400

    Fix dining style typo

--- app/controllers/api/dining_style_controller.rb
+++ app/controllers/api/dining_style_controller.rb
@@ -1,11 +1,13 @@
 class Api::DiningStyleController < ApplicationController
 
-  def show
-    @dining_style = DiningStyle.find(params[:id]).restaurants
+  # def show
+  #   @dining_style = DiningStyle.find(params[:id]).restaurants
+  #
+  # end
+  #
+  # private
+  # def dining_style_params
+  # end
 
-  end
 
-  private
-  def dining_style_params
-    params.require(:dining_style).
 end
