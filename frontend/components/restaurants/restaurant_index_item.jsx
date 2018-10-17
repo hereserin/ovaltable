f360919f1c8dd1796bff7ceaafdee434fdf08d40
Erin Marshall <erin.e.mars@gmail.com>
2018-10-17 16:56:10 -0400

    Change thumbnail img on restaurant index page to be backround image on div

--- frontend/components/restaurants/restaurant_index_item.jsx
+++ frontend/components/restaurants/restaurant_index_item.jsx
@@ -1,33 +1,38 @@
-import React from 'react';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { withRouter } from "react-router-dom";
 
-const RestaurantIndexItem = (props) => {
-
+const RestaurantIndexItem = props => {
   // if(props. == 0) return <div></div>;
 
   const thumbId = props.restaurant.photos[0];
   let thumbnail = window.staticImages.defaultRestaurant;
 
-  if ( thumbId ) {
+  if (thumbId) {
     thumbnail = props.photos[thumbId].photoUrl;
   }
+  // <img className="restaurant_index_item_thumbnail" src={thumbnail} />
 
-
-    return (
-      <li className='restaurant_index_item'>
-        <img className='restaurant_index_item_thumbnail' src={thumbnail} />
-        <div>
-          <h3 onClick={()=>{props.history.push(`/restaurants/${props.restaurant.id}`)}}>
-            {props.restaurant.restaurant_name}
-          </h3>
-            {props.restaurant.restaurant_description}
-        </div>
-      </li>
-    );
-
-
-}
-
-
+  return (
+    <li className="restaurant_index_item">
+      <div
+        className="restaurant_index_item_thumbnail"
+        onClick={() => {
+          props.history.push(`/restaurants/${props.restaurant.id}`);
+        }}
+        style={{ backgroundImage: `url(${thumbnail})` }}
+      />
+      <div>
+        <h3
+          onClick={() => {
+            props.history.push(`/restaurants/${props.restaurant.id}`);
+          }}
+        >
+          {props.restaurant.restaurant_name}
+        </h3>
+        {props.restaurant.restaurant_description}
+      </div>
+    </li>
+  );
+};
 
 export default withRouter(RestaurantIndexItem);
