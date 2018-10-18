2017a20f4de6d70797c68d282239d2981a846f3c
Erin Marshall <erin.e.mars@gmail.com>
2018-10-18 18:14:11 -0400

    Add icons with links to github, portfolio site, etc to footer

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import GreetingContainer from './greeting/greeting_container';
-import LoginFormContainer from './session/login_form_container';
-import SignupFormContainer from './session/signup_form_container';
-import RestaurantIndexContainer from './restaurants/restaurant_index_container';
-import ReservationIndexContainer from './reservations/reservation_index_container';
-import RestaurantShowPageContainer from './restaurants/restaurant_show/restaurant_show_container';
-import ReservationShowPage from './reservations/reservations_show';
-import Modal from './modal/modal';
-import NavBar from './nav_bar/nav_bar';
-import HomepageSearchSection from './search_bar/homepage_search_section';
-import RestaurantIndexSearchSection from './search_bar/restaurant_index_search_section';
-import { AuthRoute, ProtectedRoute } from './../util/route_util.jsx';
-import { Route, Switch } from 'react-router-dom';
-
+import React from "react";
+import { Provider } from "react-redux";
+import GreetingContainer from "./greeting/greeting_container";
+import LoginFormContainer from "./session/login_form_container";
+import SignupFormContainer from "./session/signup_form_container";
+import RestaurantIndexContainer from "./restaurants/restaurant_index_container";
+import ReservationIndexContainer from "./reservations/reservation_index_container";
+import RestaurantShowPageContainer from "./restaurants/restaurant_show/restaurant_show_container";
+import ReservationShowPage from "./reservations/reservations_show";
+import Modal from "./modal/modal";
+import NavBar from "./nav_bar/nav_bar";
+import HomepageSearchSection from "./search_bar/homepage_search_section";
+import RestaurantIndexSearchSection from "./search_bar/restaurant_index_search_section";
+import { AuthRoute, ProtectedRoute } from "./../util/route_util.jsx";
+import { Route, Switch } from "react-router-dom";
+import Footer from "./footer/footer";
 
 const App = () => {
   return (
@@ -22,29 +22,35 @@ const App = () => {
       <header>
         <NavBar />
         <Switch>
-
-          <Route exact path='/restaurants' component={RestaurantIndexSearchSection} />
+          <Route
+            exact
+            path="/restaurants"
+            component={RestaurantIndexSearchSection}
+          />
         </Switch>
-        <Route exact path='/' component={HomepageSearchSection} />
+        <Route exact path="/" component={HomepageSearchSection} />
       </header>
-      <main className='main-component'>
+      <main className="main-component">
         <Route exact path="/restaurants" component={RestaurantIndexContainer} />
 
-        <Route path="/restaurants/:restaurantId" component={RestaurantShowPageContainer} />
+        <Route
+          path="/restaurants/:restaurantId"
+          component={RestaurantShowPageContainer}
+        />
 
         <Route path="/reservations" component={ReservationShowPage} />
 
-        <Route path="/user/:userId/reservations" component={ReservationIndexContainer} />
-
+        <Route
+          path="/user/:userId/reservations"
+          component={ReservationIndexContainer}
+        />
       </main>
+      <Footer />
     </div>
   );
 };
 
-
-
 // commented out stuff:
-
 
 // <Route path='/login/' component={HomepageSearchSection} />
 // <Route path='/signup/' component={HomepageSearchSection} />
@@ -52,6 +58,5 @@ const App = () => {
 // removing these routes & replacing with ui modal:
 // <AuthRoute path='/login' component={LoginFormContainer} />
 // <AuthRoute path='/signup' component={SignupFormContainer} />
-
 
 export default App;
--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -1,1 +1,38 @@
-// footer here 
+import React from "react";
+
+const Footer = props => {
+  return (
+    <footer>
+      <div className="my-link-icon-holder">
+        <p>About Me:</p>
+        <ul className="my-link-icons">
+          <li>
+            <a href="https://github.com/hereserin">
+              <i class="fab fa-github" />
+            </a>
+          </li>
+
+          <li>
+            <a href="https://www.linkedin.com/in/erin-e-m/">
+              <i class="fab fa-linkedin-in" />
+            </a>
+          </li>
+
+          <li>
+            <a href="https://angel.co/erin-marshall-1">
+              <i class="fab fa-angellist" />
+            </a>
+          </li>
+
+          <li>
+            <a href="http://erin-e-marshall.com/">
+              <i class="fas fa-user-circle" />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
