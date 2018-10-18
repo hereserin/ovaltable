2077a452d8480d71f67db5d7e0652042f27294f9
Erin Marshall <erin.e.mars@gmail.com>
2018-10-18 18:26:29 -0400

    Add blurb about OvalTable to the footer

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,6 +3,14 @@ import React from "react";
 const Footer = props => {
   return (
     <footer>
+      <div>
+        <p>
+          OvalTable is a single-page web application built on Ruby on Rails with
+          React.js
+          <br />
+          It is inspired by the popular OpenTable website.
+        </p>
+      </div>
       <div className="my-link-icon-holder">
         <p>About Me:</p>
         <ul className="my-link-icons">
