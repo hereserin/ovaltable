a686f233cd30a39625b883a53fde5aaeae4d9f74
Erin Marshall <erin.e.mars@gmail.com>
2018-10-25 17:17:07 -0400

    format module for reservation show

--- frontend/components/reservations/reservations_show.jsx
+++ frontend/components/reservations/reservations_show.jsx
@@ -5,9 +5,9 @@ import { clearReservation } from "./../../actions/reservation_show_actions";
 import { openModal, closeModal } from "./../../actions/modal_actions";
 
 const ReservationShowPage = props => {
-  const message = `Your upcoming reservation at ${
-    props.reservation.restaurant_name
-  }:`;
+  const message = props.specialMessage
+    ? props.specialMessage
+    : `Your upcoming reservation at ${props.reservation.restaurant_name}:`;
   const peopleOrPerson =
     props.reservation.party_size > 1 ? " people" : " person";
 
@@ -30,16 +30,19 @@ const ReservationShowPage = props => {
             <span className="show-photo-span">
               <h2>{message}</h2>
 
-              <div>
-                <h3>GUESTS</h3>
+              <div className="reservation-show-modal-info">
+                <h3>GUESTS: </h3>
                 <p>
                   {props.reservation.party_size}
                   {peopleOrPerson}
                 </p>
+                <br />
                 <h3>DATE</h3>
                 <p>{props.reservation.date}</p>
+                <br />
                 <h3>TIME</h3>
                 <p>{props.reservation.time}</p>
+                <br />
                 <h3>RESTAURANT</h3>
                 <p>{props.reservation.restaurant_name}</p>
               </div>
