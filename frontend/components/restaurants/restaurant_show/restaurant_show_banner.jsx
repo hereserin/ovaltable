1cbb712a26e6a3d92448c7d1d09db4eb250c9d14
Erin Marshall <erin.e.mars@gmail.com>
2018-10-31 12:16:47 -0400

    Change default banner image for restaurant show page

--- frontend/components/restaurants/restaurant_show/restaurant_show_banner.jsx
+++ frontend/components/restaurants/restaurant_show/restaurant_show_banner.jsx
@@ -1,12 +1,18 @@
-import React from 'react';
+import React from "react";
 
-const RestaurantShowBanner = (props) => {
+const RestaurantShowBanner = props => {
   let bannerImage = undefined;
-  if (!props.restaurant.banner_image) {}
-    bannerImage =  <div className="main-restaurant-show-banner" style={{backgroundImage: `url(${window.staticImages.defaultRestaurantBanner})`}} />
-  return (
-    bannerImage
+  if (!props.restaurant.banner_image) {
+  }
+  bannerImage = (
+    <div
+      className="main-restaurant-show-banner"
+      style={{
+        backgroundImage: `url(${window.staticImages.defaultRestaurantBanner2})`
+      }}
+    />
   );
+  return bannerImage;
 };
 
 export default RestaurantShowBanner;
