f2a9f766de0aecdc6b2f13f8fd71933f4dd34232
Erin Marshall <erin.e.mars@gmail.com>
2018-11-01 16:35:43 -0400

    Update user id name in route util

--- frontend/util/route_util.jsx
+++ frontend/util/route_util.jsx
@@ -1,46 +1,53 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { Redirect, Route, withRouter } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import { Redirect, Route, withRouter } from "react-router-dom";
 
 const Auth = ({ component: Component, path, loggedIn, exact }) => {
-  return ( <Route path={path} exact={exact} render={(props) => {
-    return (
-      !loggedIn ? (
-        <Component {...props} />
-        ) : (
-          <Redirect to="/" />
-        )
-      )
-    }
-  }/>);
-}
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={props => {
+        return !loggedIn ? <Component {...props} /> : <Redirect to="/" />;
+      }}
+    />
+  );
+};
 
 const Protected = ({ component: Component, path, loggedIn, exact }) => {
   // <Redirect to="/login" />
-  return ( <Route path={path} exact={exact} render={(props) => {
-    return (
-      loggedIn ? (
-        <Component {...props} />
-        ) : (
-          <div>
-          </div>
-        )
-      )
-    }
-  }/>);
-}
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={props => {
+        return loggedIn ? <Component {...props} /> : <div />;
+      }}
+    />
+  );
+};
 
-const mapStatetoProps = (state) => {
+const mapStatetoProps = state => {
   return {
-    loggedIn: Boolean(state.session.currentUserId),
+    loggedIn: Boolean(state.session.id)
   };
 };
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = dispatch => {
   return {
-    openModal: () => dispatch(openModal("login")),
+    openModal: () => dispatch(openModal("login"))
   };
 };
 
-export const AuthRoute = withRouter(connect(mapStatetoProps, null)(Auth));
-export const ProtectedRoute = withRouter(connect(mapStatetoProps, mapDispatchToProps)(Protected));
+export const AuthRoute = withRouter(
+  connect(
+    mapStatetoProps,
+    null
+  )(Auth)
+);
+export const ProtectedRoute = withRouter(
+  connect(
+    mapStatetoProps,
+    mapDispatchToProps
+  )(Protected)
+);
