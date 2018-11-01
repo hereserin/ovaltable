16146819cbbe651bfc2f4b51574fbb5c1584d736
Erin Marshall <erin.e.mars@gmail.com>
2018-11-01 16:32:31 -0400

    Fix photo form modul

--- frontend/components/photos/photo_form.jsx
+++ frontend/components/photos/photo_form.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import { connect } from 'react-redux';
-import { clearPhoto } from './../../actions/photo_show_actions';
-import { submitPhoto } from './../../actions/photos_actions';
-import { withRouter } from 'react-router-dom';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { clearPhoto } from "./../../actions/photo_show_actions";
+import { submitPhoto } from "./../../actions/photos_actions";
+import { withRouter } from "react-router-dom";
+import { closeModal } from "./../../actions/modal_actions";
 
 class PhotoForm extends React.Component {
-
   constructor(props) {
     super(props);
     this.handleSubmit = this.handleSubmit.bind(this);
@@ -21,12 +20,12 @@ class PhotoForm extends React.Component {
       photoUrl: null,
       caption: "",
       instructions: "Choose a file to upload",
-      loadSuccess: false,
+      loadSuccess: false
     };
   }
 
   handleInput(e) {
-    this.setState({caption: e.currentTarget.value});
+    this.setState({ caption: e.currentTarget.value });
   }
 
   handleFile(e) {
@@ -35,42 +34,45 @@ class PhotoForm extends React.Component {
 
     fileReader.onloadend = () => {
       this.setState({
-          photoFile: file,
-          photoUrl: fileReader.result
+        photoFile: file,
+        photoUrl: fileReader.result
       });
     };
     if (file) {
       fileReader.readAsDataURL(file);
     } else {
-      this.setState({ photoFile: null, photoUrl: "", caption: ""});
+      this.setState({ photoFile: null, photoUrl: "", caption: "" });
     }
   }
 
-  componentDidMount(){
-
-  }
-
+  componentDidMount() {}
 
   handleSubmit(e) {
     e.preventDefault();
     const formData = new FormData();
-    formData.append('photo[photo_url]', this.state.photoUrl);
-    formData.append('photo[restaurant_id]', this.state.restaurantId);
-    formData.append('photo[caption]', this.state.caption);
+    formData.append("photo[photo_url]", this.state.photoUrl);
+    formData.append("photo[restaurant_id]", this.state.restaurantId);
+    formData.append("photo[caption]", this.state.caption);
     if (this.state.photoFile) {
-      formData.append('photo[pic]', this.state.photoFile);
+      formData.append("photo[pic]", this.state.photoFile);
     }
 
     this.props.submitPhoto(formData).then(
-      (response) => {
+      response => {
         this.setState({ loadSuccess: true });
-        this.setState({ instructions: "Image has been posted! You may choose another file to upload",  photoFile: null, photoUrl: "", caption: ""});
+        this.setState({
+          instructions:
+            "Image has been posted! You may choose another file to upload",
+          photoFile: null,
+          photoUrl: "",
+          caption: ""
+        });
       },
-      (response) => {
-         this.setState({ instructions: "Couldn't Load Image File."  });
+      response => {
+        this.setState({ instructions: "Couldn't Load Image File." });
       }
     );
-    }
+  }
 
   render() {
     let preview = null;
@@ -78,47 +80,55 @@ class PhotoForm extends React.Component {
       preview = (
         <div>
           <h4>Preview </h4>
-          <br></br>
-          <img className='photo-form-preview-image' src={this.state.photoUrl} />
+          <br />
+          <img className="photo-form-preview-image" src={this.state.photoUrl} />
         </div>
       );
     }
 
     return (
-        <div>
-            <span className='photo-form-modal'>
-              <span className='photo-form-modal-screen' onClick={() => {
-                  props.history.push('/');
-              }}>
-              <span className='photo-form-modal-box' onClick={e => {
-                    e.stopPropagation();
-              }}>
-              <span className='photo-form-span'>
+      <div>
+        <span className="photo-form-modal">
+          <span
+            className="photo-form-modal-screen"
+            onClick={() => {
+              this.props.closeModal();
+            }}
+          >
+            <span
+              className="photo-form-modal-box"
+              onClick={e => {
+                e.stopPropagation();
+              }}
+            >
+              <span className="photo-form-span">
                 <h2>Add a Photo</h2>
                 <p>{this.state.instructions}</p>
                 <form onSubmit={this.handleSubmit}>
-
-
-                  <input type="file"
-                    className='photo-form-file-input'
-                    onChange={this.handleFile}/>
-                  <br></br>
+                  <input
+                    type="file"
+                    className="photo-form-file-input"
+                    onChange={this.handleFile}
+                  />
+                  <br />
 
                   <label htmlFor="caption">
                     <h4>Describe the Image: </h4>
                   </label>
-                  <br></br>
-                  <textarea rows="2" cols="45"
-                    className='photo-form-textarea'
+                  <br />
+                  <textarea
+                    rows="2"
+                    cols="45"
+                    className="photo-form-textarea"
                     id="caption"
                     value={this.state.caption}
-                    onChange={this.handleInput}/>
-                  <br></br>
-                    {preview}
+                    onChange={this.handleInput}
+                  />
+                  <br />
+                  {preview}
 
-                  <button className='photo-form-btn'> Post Photo</button>
+                  <button className="photo-form-btn"> Post Photo</button>
                 </form>
-
               </span>
             </span>
           </span>
@@ -126,26 +136,29 @@ class PhotoForm extends React.Component {
       </div>
     );
   }
-
 }
 
 const mapStateToProps = (state, ownProps) => {
-
-  return ({
+  return {
     showPhoto: state.ui.showPhoto,
-    restaurant: ownProps.history.location.pathname.split("/")[2],
-  });
+    restaurant: ownProps.history.location.pathname.split("/")[2]
+  };
 };
 
-const mapDispatchToProps = (dispatch) => {
-
-  return ({
+const mapDispatchToProps = dispatch => {
+  return {
     // clearPhoto: () => dispatch(clearPhoto()),
-    submitPhoto: (photo) => dispatch(submitPhoto(photo))
-  });
+    submitPhoto: photo => dispatch(submitPhoto(photo)),
+    closeModal: modal => dispatch(closeModal())
+  };
 };
 
-export default withRouter(connect(mapStateToProps, mapDispatchToProps)(PhotoForm));
+export default withRouter(
+  connect(
+    mapStateToProps,
+    mapDispatchToProps
+  )(PhotoForm)
+);
 
 //
 // <input type="text"
