e2873f910f2f0c7c05043bf0fa26fe07a80e051b
Erin Marshall <erin.e.mars@gmail.com>
2018-11-02 12:26:17 -0400

    Make modul close once a user has signed in

--- frontend/reducers/modal_reducer.js
+++ frontend/reducers/modal_reducer.js
@@ -1,15 +1,15 @@
-import {
-  OPEN_MODAL,
-  CLOSE_MODAL
-} from './../actions/modal_actions';
+import { OPEN_MODAL, CLOSE_MODAL } from "./../actions/modal_actions";
+import { RECEIVE_CURRENT_USER } from "./../actions/session_actions";
 
-const modalReducer = (state=[], action) => {
+const modalReducer = (state = [], action) => {
   Object.freeze(state);
   switch (action.type) {
     case OPEN_MODAL:
       return action.modal;
     case CLOSE_MODAL:
-      return null
+      return null;
+    case RECEIVE_CURRENT_USER:
+      return null;
     default:
       return state;
   }
