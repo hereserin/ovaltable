f59c7e58d341a6f802ffad9ed7f088d3a91c6ed8
Erin Marshall <erin.e.mars@gmail.com>
2018-11-05 13:53:30 -0500

    Add user_id to review show json to get that info when new review is posted

--- frontend/components/reviews/reviews_index.jsx
+++ frontend/components/reviews/reviews_index.jsx
@@ -1,40 +1,41 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import ReviewForm from './review_form';
-import ReviewIndexItem from './review_index_item';
-import { ProtectedRoute } from './../../util/route_util.jsx';
-import { withRouter } from 'react-router-dom';
+import React from "react";
+import { connect } from "react-redux";
+import ReviewForm from "./review_form";
+import ReviewIndexItem from "./review_index_item";
+import { ProtectedRoute } from "./../../util/route_util.jsx";
+import { withRouter } from "react-router-dom";
 
-const ReviewsIndex = (props) => {
+const ReviewsIndex = props => {
+  let reviewsList = props.reviewIds.map(indexId => {
+    return (
+      <ReviewIndexItem
+        key={indexId}
+        review={props.reviews[indexId]}
+        reviewAuthor={props.users[props.reviews[indexId].user_id]}
+      />
+    );
+  });
 
-      let reviewsList = props.reviewIds.map((indexId, idx) => {
-          return (
-            <ReviewIndexItem key={idx} review={props.reviews[indexId]} />
-          );
-        })
+  return (
+    <div className="reviews-list">
+      <h2>Reviews</h2>
+      <div>{reviewsList}</div>
 
-      return (
-        <div className='reviews-list'>
-          <h2>Reviews</h2>
-          <div>
-            {reviewsList}
-          </div>
-
-          <ProtectedRoute component={() => {
-              return (
-                <ReviewForm />
-              );
-          }} />
-        </div>
-      );
-}
-
+      <ProtectedRoute
+        component={() => {
+          return <ReviewForm />;
+        }}
+      />
+    </div>
+  );
+};
 
 const mapStateToProps = (state, ownProps) => {
   const restaurantId = ownProps.location.pathname.split("/")[2];
   return {
-      reviews: state.entities.reviews,
-      reviewIds: state.entities.restaurants[restaurantId].reviews
+    reviews: state.entities.reviews,
+    reviewIds: state.entities.restaurants[restaurantId].reviews,
+    users: state.entities.users
   };
 };
 // HOW DO YOU GET A WILDCARD FROM A URL?
