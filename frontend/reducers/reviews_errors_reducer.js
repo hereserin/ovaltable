9477344946ff37352683c9e05c12a299efb26804
Erin Marshall <erin.e.mars@gmail.com>
2018-11-06 21:36:35 -0500

    Have review errors clear when restaurant or reservation is received

--- frontend/reducers/reviews_errors_reducer.js
+++ frontend/reducers/reviews_errors_reducer.js
@@ -2,6 +2,14 @@ import {
   RECEIVE_REVIEW_ERRORS,
   RECEIVE_REVIEW
 } from "./../actions/reviews_actions";
+import {
+  RECEIVE_RESTAURANT,
+  RECEIVE_RESTAURANTS
+} from "./../actions/restaurant_actions";
+import {
+  RECEIVE_RESERVATION,
+  RECEIVE_RESERVATIONS
+} from "./../actions/reservations_actions";
 
 const initialState = {};
 
@@ -11,6 +19,10 @@ const reviewErrorsReducer = (state = initialState, action) => {
     case RECEIVE_REVIEW_ERRORS:
       return action.errors;
     case RECEIVE_REVIEW:
+    case RECEIVE_RESTAURANT:
+    case RECEIVE_RESTAURANTS:
+    case RECEIVE_RESERVATION:
+    case RECEIVE_RESERVATIONS:
       return initialState;
     default:
       return state;
