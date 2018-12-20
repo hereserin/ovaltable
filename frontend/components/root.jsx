23ff491d4a8a3e6f33f30442dd8a573be4ad1ce8
Erin Marshall <erin.e.mars@gmail.com>
2018-12-19 19:07:06 -0500

    Update root component

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,19 +1,16 @@
-import React from 'react';
-import { Provider } from 'react-redux';
-import { HashRouter } from 'react-router-dom';
-import App from './app';
+import React from "react";
+import { Provider } from "react-redux";
+import { HashRouter } from "react-router-dom";
+import App from "./app";
 
-
-// give store as argument:
 const Root = ({ store }) => {
   return (
-    <Provider store={ store }>
+    <Provider store={store}>
       <HashRouter>
-      <App />
+        <App />
       </HashRouter>
     </Provider>
   );
 };
-
 
 export default Root;
